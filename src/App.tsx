04f28edc45b2fec799256a59d5a6c93f7cfac11c
Slim-Shary <shahryarmon@gmail.com>
2024-03-14 13:18:41 -0700

    add expense list component and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+import { ExpenseList } from './components/ExpenseList';
+import { expenseData } from './data/expenses';
+
 function App() {
-  return <div></div>;
+  const [expenses, setExpenses] = useState(expenseData);
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/expenses.ts
@@ -0,0 +1,6 @@
+export const expenseData = [
+  { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+  { id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+  { id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
+  { id: 4, description: 'ddd', amount: 10, category: 'Utilities' },
+];
