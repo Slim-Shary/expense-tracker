985d1941a32477198d19ab84682450a16fada8e0
Slim-Shary <shahryarmon@gmail.com>
2024-03-14 13:57:54 -0700

    add expense form using zod and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { ExpenseList } from './components/ExpenseList';
 import { expenseData } from './data/expenses';
-import { ExpenseFilter } from './components/ExpenseFilter';
+import ExpenseFilter from './components/ExpenseFilter';
+import ExpenseForm from './components/ExpenseForm';
 
 function App() {
   const [expenses, setExpenses] = useState(expenseData);
@@ -13,6 +14,13 @@ function App() {
 
   return (
     <div>
+      <div className='mb-5'>
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
       <div className='mb-3'>
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
--- src/components/ExpenseFilter.tsx
+++ src/components/ExpenseFilter.tsx
@@ -1,16 +1,23 @@
-type Props = {
+import categories from '../data/categories';
+
+interface Props {
   onSelectCategory: (category: string) => void;
-};
-export const ExpenseFilter = ({ onSelectCategory }: Props) => {
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <select
       className='form-select'
       onChange={(event) => onSelectCategory(event.target.value)}
     >
-      <option value=''>All Categories</option>
-      <option value='Groceries'>Groceries</option>
-      <option value='Utilities'>Utilities</option>
-      <option value='Entertainment'>Entertainment</option>
+      <option value=''>All categories</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
     </select>
   );
 };
+
+export default ExpenseFilter;
