923175366f3b60c6b2700905f8cccc7d74614376
Slim-Shary <shahryarmon@gmail.com>
2024-03-14 13:32:54 -0700

    add filtering to the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import { ExpenseList } from './components/ExpenseList';
 import { expenseData } from './data/expenses';
+import { ExpenseFilter } from './components/ExpenseFilter';
 
 function App() {
   const [expenses, setExpenses] = useState(expenseData);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className='mb-3'>
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
